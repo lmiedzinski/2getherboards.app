cb0d803edc9f61fc139203969229f8bdd74f1536
lmiedzinski <lukasz.miedzinski.1@gmail.com>
2024-03-02 16:52:10 +0100

    Added unit test for pipeline check

---
+++ Backend/tests/TogetherBoardsApp.Backend.Domain.UnitTests/TestForPipeline.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TogetherBoardsApp.Backend.Domain.UnitTests;
+
+public class TestForPipeline
+{
+    [Fact]
+    public void Test_For_Pipeline()
+    {
+        Assert.True(true);
+    }
+}
